7d9bf26173e4110c7e2bd22004aa901ddafb77c8
Sean Parker <sean.parker@x-labsystems.co.uk>
2019-08-20 16:38:49 +0100

    added and tested getArea function

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -6,18 +6,23 @@ namespace cSharp
     {
         static void Main(string[] args)
         {
-            int sideA,sideB,sideC;
+            double sideA,sideB,sideC;
             Program user = new Program();
             sideA = user.getSide("side A");
             sideB = user.getSide("side B");
             sideC = user.getSide("side C");
 
-            trianglePerimeter = user.getPerimeter(sideA, sideB, side)
+            double trianglePerimeter = user.getPerimeter(sideA, sideB, sideC);
+            double traingleArea = user.getArea(sideA, sideB, sideC);
+
+            
+            Console.WriteLine("Perimeter:" + trianglePerimeter);
+            Console.WriteLine("Area:" + traingleArea);
         }
 
-        public int getSide(string sideName)
+        public double getSide(string sideName)
         {
-            int side;
+            double side;
             Console.WriteLine("Please input value of " + sideName);
             side = Convert.ToInt32(Console.ReadLine());
 
@@ -25,10 +30,23 @@ namespace cSharp
 
         }
 
-        public int getPerimeter(int sideA, int sideB, int sideC)
+        public double getPerimeter(double sideA, double sideB, double sideC)
         {
-            int perimeter = sideA + sideB + sideC;
+            double perimeter = sideA + sideB + sideC;
             return perimeter;
+        }
+
+        public double getArea(double sideA, double sideB, double sideC)
+        {
+            double area;
+            double halfPerimeter = this.getPerimeter(sideA, sideB, sideC)/2;
+            //this method of calculating the area is herons formula 
+            area = halfPerimeter*(halfPerimeter - sideA) * (halfPerimeter-sideB) * (halfPerimeter-sideC);
+
+            //square root
+            area = Math.Pow(area,0.5);
+            return area;
+
         }
     }
 }
