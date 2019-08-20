03113afc242fc71998cfa7ece4c37c01f90a1461
Sean Parker <sean.parker@x-labsystems.co.uk>
2019-08-20 16:23:53 +0100

    Add the getSide function and getPerimeter function

---
+++ cSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA,sideB,sideC;
+            Program user = new Program();
+            sideA = user.getSide("side A");
+            sideB = user.getSide("side B");
+            sideC = user.getSide("side C");
+
+            trianglePerimeter = user.getPerimeter(sideA, sideB, side)
+        }
+
+        public int getSide(string sideName)
+        {
+            int side;
+            Console.WriteLine("Please input value of " + sideName);
+            side = Convert.ToInt32(Console.ReadLine());
+
+            return side;
+
+        }
+
+        public int getPerimeter(int sideA, int sideB, int sideC)
+        {
+            int perimeter = sideA + sideB + sideC;
+            return perimeter;
+        }
+    }
+}
