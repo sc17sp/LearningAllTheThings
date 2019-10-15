84624e4b77179e4761b574397fe20c5481ab0a2c
Sean Parker <sean.parker@x-labsystems.co.uk>
2019-10-15 11:00:46 +0100

    Created new euler project

---
+++ eulerProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace eulerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+            int sum = test.fibonacciFormula(1, 2, 2);
+            Console.WriteLine(sum);
+        }
+
+        public int fibonacciFormula(int first, int second, int sum)
+        {
+            int next = first + second;
+
+            
+
+            if (next > 89)
+            {
+                return sum;
+            }
+
+            if (next % 2 == 0)
+            {
+                sum = sum + next;
+
+                sum = fibonacciFormula(second, next, sum);
+            }
+            else
+            {
+                sum = fibonacciFormula(second, next, sum);
+            }
+
+            return sum;
+        }
+    }
+}
