96540af8993ba0988d410d392b67688015a53d75
Sean Parker <sean.parker@x-labsystems.co.uk>
2020-02-03 15:07:55 +0000

    done some more

--- MVC/webApp/webApp/Controllers/StudentController.cs
+++ MVC/webApp/webApp/Controllers/StudentController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webApp.Models;
 
 namespace webApp.Controllers
 {
     public class StudentController : Controller
     {
         // GET: Student
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is a test";
+
+            var studentList = new List<Student> {
+                new Student() { StudentID = 1, StudentName ="John", Age = 18},
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentID = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentID = 4, StudentName = "Rob" , Age = 19 }
+            };
+            return View(studentList);
         }
     }
 }
---
+++ MVC/webApp/webApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApp.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
