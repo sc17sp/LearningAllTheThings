9a0006e3120d63e6ea25045f1d5777946d477a1b
Sean Parker <sean.parker@x-labsystems.co.uk>
2020-02-03 14:37:14 +0000

    Created student controller

---
+++ MVC/webApp/webApp/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public string Index()
+        {
+            return "This is a test";
+        }
+    }
+}
